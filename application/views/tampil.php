c959563ff16a4089e9c30c10097759101d49c0e1
windasiti18 <you@examplewindasiti18@gmail.com>
2022-09-20 16:25:51 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>latihan pertama</title>
+</head>
+<body>
+    <p>latihan pertama saya belajar framework codeigniter </p>
+    <p>framework codeigneter sangat mudah dipelajari</p>
+     
+</body>
+</html>
